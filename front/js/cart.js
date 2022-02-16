409d9fc9f80baeb2a567a627ce4259c544f73e90
Achilleai <87209387+Achilleai@users.noreply.github.com>
2022-02-16 20:43:15 +0100

    update cart.js

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,2 +1,13 @@
 const getCart = JSON.parse(localStorage.getItem("product"));
-console.log(getCart, "id", getCart[0].id)
+for (let cartElt of getCart) {
+    let getId = cartElt.id;
+    console.log(getId)
+    let getFetch = fetch("http://localhost:3000/api/products/" + getId).then(response => response.json()).then(couch =>{cart(couch)})
+    function cart(product){
+      let productArticle = document.createElement("article");
+      document.querySelector("#cart__items").appendChild(productArticle);
+      productArticle.className = "cart__item";
+      productArticle.textContent = product.name;
+    }
+    
+}
