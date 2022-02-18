72fa7f9833021509a0efc7edd6577d52b3e691ef
ach42 <87209387+ach42@users.noreply.github.com>
2022-02-18 15:54:15 +0100

    Update cart.js

--- front/js/cart.js
+++ front/js/cart.js
@@ -14,11 +14,47 @@ for (let cartElt of cart) {
       productArticle.className = "cart__item";
       productArticle.setAttribute('data-id', cartElt.id);
       productArticle.setAttribute('data-color', cartElt.color);
+      // création div cart__item__img
       let productDivImg = document.createElement("div");
       productDivImg.className = "cart__item__img";
-      productArticle.appendChild(productDivImg)
+      productArticle.appendChild(productDivImg);
+      // image produit
       let productImg = document.createElement("img");
       productImg.src = couches.imageUrl;
-      productDivImg.appendChild(productImg)
+      productImg.alt = couches.name;
+      productDivImg.appendChild(productImg);
+      // création div cart__item__content
+      let productDivContent = document.createElement("div");
+      productDivContent.className = "cart__item__content";
+      productArticle.appendChild(productDivContent);
+      // création div cart__item__content__description
+      let productDivContentDesc = document.createElement("div");
+      productDivContentDesc.className = "cart__item__content__description";
+      productDivContent.appendChild(productDivContentDesc);
+      // produit content
+      let productTitleContent = document.createElement("h2");
+      productTitleContent.textContent = couches.name;
+      productDivContentDesc.appendChild(productTitleContent);
+      let productColor = document.createElement("p");
+      productColor.textContent = cartElt.color;
+      productDivContentDesc.appendChild(productColor);
+      let productPrice = document.createElement("p");
+      productPrice.textContent = couches.price + "€";
+      productDivContentDesc.appendChild(productPrice);
+      // création cart__item__content__settings
+      let settingsDivItemContent = document.createElement("div");
+      settingsDivItemContent.className = "cart__item__content__settings";
+      productDivContent.appendChild(settingsDivItemContent);
+      // création div cart__item__content__settings__quantity
+      let settingsDivItemQuantity = document.createElement("div");
+      settingsDivItemQuantity.className = "cart__item__content__settings__quantity";
+      settingsDivItemContent.appendChild(settingsDivItemQuantity)
+      // Quantité
+      let productQuantity = document.createElement("p");
+      productQuantity.textContent = "Qté : " + cartElt.quantity;
+      settingsDivItemQuantity.appendChild(productQuantity);
+      let productQuantityInput = document.createElement("input", Number);
+      productQuantityInput.className = "itemQuantity";
+      settingsDivItemQuantity.appendChild(productQuantityInput);
     }
 }
