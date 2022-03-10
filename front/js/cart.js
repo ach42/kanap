2ddf6fd87839f5a45aed7ffeedd6a754706b0738
Achilleai <ach42@ik.me>
2022-03-10 18:47:29 +0100

    add promise.all

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,86 +1,93 @@
+const url = "http://localhost:3000/api/products/"
+const promises = []
 const cart = JSON.parse(localStorage.getItem("product"));
-for (let cartElt of cart) {
+cart.forEach(kanap => {
+  const promise = fetch(url + kanap.id).then(res => res.json())
+  promises.push(promise)
+ });
 
-    fetch("http://localhost:3000/api/products/" + cartElt.id).then(response => response.json())
-    .then(couches => {
-      cart(couches)
-    })
+Promise.all(promises).then(kanaps => {
+  kanaps.forEach((kanap, i) => {
+    kanap.colors = cart[i].color;
+    kanap.quantity = cart[i].quantity;
+    displayProduct(kanap)
+  })
+  console.log(kanaps)
+})
 
-    // intégration
-    function cart(couches){
-      // création <article>
-      let productArticle = document.createElement("article");
-      document.querySelector("#cart__items").appendChild(productArticle);
-      productArticle.className = "cart__item";
-      productArticle.setAttribute('data-id', cartElt.id);
-      productArticle.setAttribute('data-color', cartElt.color);
-      // création div cart__item__img
-      let productDivImg = document.createElement("div");
-      productDivImg.className = "cart__item__img";
-      productArticle.appendChild(productDivImg);
-      // image produit
-      let productImg = document.createElement("img");
-      productImg.src = couches.imageUrl;
-      productImg.alt = couches.name;
-      productDivImg.appendChild(productImg);
-      // création div cart__item__content
-      let productDivContent = document.createElement("div");
-      productDivContent.className = "cart__item__content";
-      productArticle.appendChild(productDivContent);
-      // création div cart__item__content__description
-      let productDivContentDesc = document.createElement("div");
-      productDivContentDesc.className = "cart__item__content__description";
-      productDivContent.appendChild(productDivContentDesc);
-      // produit content
-      let productTitleContent = document.createElement("h2");
-      productTitleContent.textContent = couches.name;
-      productDivContentDesc.appendChild(productTitleContent);
-      let productColor = document.createElement("p");
-      productColor.textContent = cartElt.color;
-      productDivContentDesc.appendChild(productColor);
-      let productPrice = document.createElement("p");
-      productPrice.textContent = couches.price + "€";
-      productDivContentDesc.appendChild(productPrice);
-      // création cart__item__content__settings
-      let settingsDivItemContent = document.createElement("div");
-      settingsDivItemContent.className = "cart__item__content__settings";
-      productDivContent.appendChild(settingsDivItemContent);
-      // création div cart__item__content__settings__quantity
-      let settingsDivItemQuantity = document.createElement("div");
-      settingsDivItemQuantity.className = "cart__item__content__settings__quantity";
-      settingsDivItemContent.appendChild(settingsDivItemQuantity)
-      // Quantité
-      let productQuantity = document.createElement("p");
-      productQuantity.textContent = "Qté : " + cartElt.quantity;
-      settingsDivItemQuantity.appendChild(productQuantity);
-      let productQuantityInput = document.createElement("input");
-      productQuantityInput.className = "itemQuantity";
-      productQuantityInput.value = cartElt.quantity;
-      productQuantityInput.setAttribute("type", "number");
-      productQuantityInput.setAttribute("min", "1");
-      productQuantityInput.setAttribute("max", "100");
-      productQuantityInput.setAttribute("name", "itemQuantity");
-      settingsDivItemQuantity.appendChild(productQuantityInput);
-      // div suppresion
-      let productDivDelete = document.createElement("div");
-      productDivDelete.className = "cart__item__content__settings__delete";
-      settingsDivItemContent.appendChild(productDivDelete);
-      let productTextDelete = document.createElement("p");
-      productTextDelete.className = "deleteItem";
-      productTextDelete.textContent = "Supprimer";
-      productDivDelete.appendChild(productTextDelete);
-    }
-}
 
-total()
+function displayProduct(kanap) {
 
-function total() {
-const totalQuantityElt = document.getElementById("totalQuantity");
-const totalQuantity = JSON.parse(cart.length);
-totalQuantityElt.textContent = totalQuantity;
-console.log(totalQuantity)
-const totalPriceElt = document.getElementById("totalPrice");
-const totalPrice = cart.reduce((total, current) => total + current.price, 0)
-console.log(totalPrice)
-totalPriceElt.textContent = totalPrice;
+    const articleElt = document.createElement("article");
+    document.querySelector("#cart__items").appendChild(articleElt);
+    articleElt.className = "cart__item";
+
+    const divImgElt = document.createElement("div");
+    divImgElt.className = "cart__item__img";
+    articleElt.appendChild(divImgElt);
+
+    const imgElt = document.createElement("img");
+    divImgElt.appendChild(imgElt)
+
+    const divItemContent = document.createElement("div");
+    divItemContent.className = "cart__item__content";
+    articleElt.appendChild(divItemContent);
+
+    const divItemDescription = document.createElement("div");
+    divItemDescription.className = "cart__item__content__description";
+    divItemContent.appendChild(divItemDescription);
+
+    const titleItem = document.createElement("h2");
+    divItemDescription.appendChild(titleItem);
+
+    const colorItem = document.createElement("p");
+    divItemDescription.appendChild(colorItem)
+
+    const priceItem = document.createElement("p");
+    divItemDescription.appendChild(priceItem)
+
+    const settingsDiv = document.createElement("div");
+    settingsDiv.className = "cart__item__content__settings";
+    divItemContent.appendChild(settingsDiv);
+
+    const settingsDivQuantity = document.createElement("div");
+    settingsDivQuantity.className = "cart__item__content__settings__quantity";
+    settingsDiv.appendChild(settingsDivQuantity)
+
+    const itemQuantity = document.createElement("p");
+    settingsDivQuantity.appendChild(itemQuantity);
+
+    const itemQuantityInput = document.createElement("input");
+    itemQuantityInput.className = "itemQuantity";
+    itemQuantityInput.setAttribute("type", "number");
+    itemQuantityInput.setAttribute("min", "1");
+    itemQuantityInput.setAttribute("max", "100");
+    itemQuantityInput.setAttribute("name", "itemQuantity");
+    settingsDivQuantity.appendChild(itemQuantityInput);
+
+    const itemDivDelete = document.createElement("div");
+    itemDivDelete.className = "cart__item__content__settings__delete";
+    settingsDiv.appendChild(itemDivDelete);
+
+    const productTextDelete = document.createElement("p");
+    productTextDelete.className = "deleteItem";
+    productTextDelete.textContent = "Supprimer";
+    itemDivDelete.appendChild(productTextDelete);
+
+    // Dynamic
+
+    imgElt.src = kanap.imageUrl;
+    imgElt.alt = kanap.name;
+
+    articleElt.setAttribute('data-id', kanap._id);
+    articleElt.setAttribute('data-color', kanap.colors);
+
+    titleItem.textContent = kanap.name;
+
+    colorItem.textContent = kanap.colors;
+
+    priceItem.textContent = kanap.price + "€";
+
+    itemQuantity.textContent = "Qté : " + kanap.quantity;
+    itemQuantityInput.value = kanap.quantity;
 }
--- front/js/product.js
+++ front/js/product.js
@@ -54,7 +54,6 @@ function addProduct(article) {
                 id: article._id,
                 color : colorElt.value,
                 quantity : quantity,
-                price: article.price,
             }      
             // Local Storage
             let cart = JSON.parse(localStorage.getItem("product")) || [];
